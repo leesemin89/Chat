743a764d63392d32d8a23e239315f72a2cd46a3b
leesemin89 <leesemin89@gmail.com>
2024-01-26 10:35:43 -0600

    na

--- src/EnterGUI.java
+++ src/EnterGUI.java
@@ -1,22 +1,2 @@
-// Client enter through GUI into chat session.
-import javax.swing.JFrame;
-
-public class EnterGUI extends JFrame{
-    private JPanel enterPane; // Creating Enter pane
-    private JButton enterButton; // Creating Enter button
-    private JTextField enterField; // Reading nickname
-    private String nickName; // Creating Nickname
-
-    public EnterGUI();{
-        enterPane(); // Creating Enter pane
-        nicknamelabel(); // Creating Nickname label
-        enterButtonEvent(); // Creating Enter button event
-        setVisible(true); // Showing GUI
-    }
-    public void enterPanel(){
-        enterPane = new JPanel();
-        enterPane.setLayout(null);
-        enterPane.setBackground(new Color(128, 255, 255));
-        setContentPane(enterPane);
-    }
+public class EnterGUI {
 }
